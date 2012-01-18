48dfceab6a5a4332caef11cf41e81b98d044b8e9
Steven Ruppert <blendmaster@gmail.com>
2012-01-18 06:25:08 -0700

    implemented prompt for new wine

--- project02/wine.rb
+++ project02/wine.rb
@@ -1,6 +1,17 @@
+#!/usr/bin/ruby
 require 'sqlite3'
 
 db = SQLite3::Database.new("wine.db")
+
+def create
+	fields = Array.new
+	puts "Enter a new wine into the database:"
+	["name","price","purchase date","drunk date","rating","comment"].each do |field|
+		print "#{field}: "
+		fields.push gets.chomp
+	end
+	return fields
+end
 
 db.query("select * from wine") do |wines|
 	wines.each do |wine|
@@ -11,3 +22,4 @@ db.query("select * from wine") do |wines|
 	end
 end
 
+db.execute( "insert into wine(name,price,purchase_date, drunk_date,rating,comments) values( $0,$1,$2,$3,$4,$5)", create);
