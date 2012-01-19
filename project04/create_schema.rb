0c13194e63516024e79fc9cfd8e044e77ea90f64
Steven Ruppert <blendmaster@gmail.com>
2012-01-19 08:23:24 -0700

    added create_schema.rb

---
+++ project04/create_schema.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/ruby
+require './magicdb.rb'
+
+db = SQLite3::Database.new( "test.db" )
+
+db.create_table :countries do
+	text :name, unique: true
+end
+
+db.create_table :vinyards do
+	text :parcel_id, unique: true
+	text :name
+	text :region
+	foreign_key_to :country
+end
+
+db.create_table :wineries do
+	text :name
+	int :tax_id, unique: true
+	foreign_key_to :vinyard
+end
+
+db.create_table :grapes do
+	text :name, unique: true
+end
+
+db.create_join_table :grapes, :wines
+
+db.create_join_table :grapes, :vinyards
+
+db.create_table :wines do
+	text :sku, unique: true
+	text :name
+	text :vintage
+	numeric :price
+	text :comment
+	int :rating
+	date :datedrunk
+	date :purchase_date
+
+	foreign_key_to :winery
+end
+
+
--- project04/magicdb.rb
+++ project04/magicdb.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'active_support/inflector' #pluralize and singularize
 
 # let's get ruby magical
 # and never write vanilla sql again
@@ -6,7 +7,15 @@ class SQLite3::Database
 	# sqlite won't bind create table statements
 	# oh well, back to string interpolation
 	def create_table name, options = {}, &block
-		execute "create table #{TableDefinition.new name,options, &block}"
+		puts "create table #{TableDefinition.new name,options, &block}"
+	end
+
+	def create_join_table *tables
+		create_table (tables.map {|t| t.to_s.pluralize}.join("_")), id: false do |t|
+			tables.each do |table|
+				t.foreign_key_to table.to_s.singularize
+			end
+		end
 	end
 end
 
@@ -22,8 +31,25 @@ class TableDefinition
 		if options[:id]
 			@columns[options[:primary_key]] = "integer primary key" 
 		end
+	
+		# add the columns in a block
 
-		instance_eval &block # add the rest of the columns
+		if block.arity > 0 # assume they want to keep their scope
+			yield self
+		else
+			instance_eval &block # sugar without keeping scope
+		end
+	end
+	
+	# oh ruby, you so fancy
+	%w[text int date datetime numeric].each do |type|
+		define_method type do |name, options ={}|
+			@columns[name] = "#{type} #{"unique" if options[:unique]}"
+		end
+	end
+
+	def foreign_key_to name
+		@columns["#{name}_id"] = "integer"
 	end
 
 	def to_s
@@ -31,14 +57,8 @@ class TableDefinition
 			@columns.map do |name, type|
 				"\"#{name}\" #{type}"
 			end.join ", "
-		})"
+		})".squeeze
 	end
 
-	# oh ruby, you so fancy
-	%w[text int datetime numeric].each do |type|
-		define_method type do |name|
-			@columns[name] = type
-		end
-	end
 
 end
